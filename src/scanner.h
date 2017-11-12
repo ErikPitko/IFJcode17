95ffc4b9a227b98cbf5a1e59a9a87d982b04fdd5
AIM666 <30591067+AIM666@users.noreply.github.com>
2017-11-12 19:34:08 +0100

    Update scanner.h

--- src/scanner.h
+++ src/scanner.h
@@ -4,11 +4,6 @@
 #include <ctype.h>
 #include <string.h>
 
-extern int ungetcharpom;
-
-
-
-
 typedef struct structToken{
 	unsigned type;
 	char *info; //Numbers will be in here too.
@@ -16,78 +11,11 @@ typedef struct structToken{
 
 
 
-int upper2lower(int c){
-	if (c >= 'A' && c <= 'Z')
-		return c+32;
-	return c;
-}
+int upper2lower(int c);
 
-int isKeyword(char* s){
-	int i = 0;
-
-	char* keyword[] = {
-		"as", //1
-		"asc",
-		"declare",
-		"dim",
-		"do",
-		"double",
-		"else",
-		"end",
-		"chr",
-		"function",
-		"if",
-		"input",
-		"integer",
-		"length",
-		"loop",
-		"print",
-		"return",
-		"scope",
-		"string",
-		"substr",
-		"then",
-		"while",
-				//reserved
-		"and",
-		"boolean",
-		"continue",
-		"elseif",
-		"exit",
-		"false",
-		"for",
-		"next",
-		"not",
-		"or",
-		"shared",
-		"static",
-		"true", //35
-		NULL//36
-	};
+int isKeyword(char* s);
 
 
-	while (keyword[i] != NULL){
-		if (strcmp(s,keyword[i]) == 0){
-			return(i+1); // vraci hodnotu daneho keywordu
-
-		}
-
-		i++;
-	}
-	return 0;// don`t even think about it
-}
-
-int isOperator(int c){
-	if(c == '+'|| c == '-'|| c == '*'|| c == 92 || c == '<' || c == '>' || c == '=' )
-		return c;
-	else return 0;
-}
+int isOperator(int c);
 // funkce pro simulaci ungetc(c,file);
-int getchar0(int c){
-	if(ungetcharpom){
-		int pom;
-		pom = ungetcharpom;
-		ungetcharpom = 0;
-		return pom;
-	}else return getchar();
-}
+int getchar0(int c);
